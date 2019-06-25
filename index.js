c39d9796026bf65535f565acdf4b25509b6087d7
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-24 23:52:01 -0400

    Added favicon and started on menu integration

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ const footer = document.querySelector(".footer");
 
 
 
-
-
-
-
-
-
-
-
-
-
 //When Learn More button is pressed, main-content is revealed and the page is scrolled to the content.
 learnMore.addEventListener("click", function() {
   mainContent.style.display = "flex";
