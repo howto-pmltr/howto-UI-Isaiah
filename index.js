c4a17ff6e8cde548b655233f37bb3221ebfd8440
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-25 10:25:51 -0400

    Added share div and smooth scrolling between divs

--- index.js
+++ index.js
@@ -3,17 +3,39 @@ const mainContent = document.querySelector(".mainContent");
 const footer = document.querySelector(".footer");
 const menu = document.querySelector("#menu");
 const menuItems = document.querySelector(".menuItems");
-
+const mainCreate = document.querySelector(".mainCreate");
+const mainFind = document.querySelector(".mainFind");
+const mainShare = document.querySelector(".mainShare");
+const cSeeMore = document.querySelector("#cSeeMore");
+const fSeeMore = document.querySelector("#fSeeMore");
+const sSeeMore = document.querySelector("#fSeeMore");
 
 //When Learn More button is pressed, main-content is revealed and the page is scrolled to the content.
-learnMore.addEventListener("click", function() {
-  mainContent.style.display = "flex";
-  footer.style.display = "flex";
-  mainContent.scrollIntoView({
-    behavior: "smooth"
-  });
+learnMore.addEventListener("click", function () {
+    mainContent.style.display = "flex";
+    footer.style.display = "flex";
+    mainContent.scrollIntoView({
+        behavior: "smooth"
+    });
 });
 
-menu.addEventListener('click', function() {
+menu.addEventListener('click', function () {
     menuItems.classList.toggle('toggleMenu');
 })
+
+// Change Content
+cSeeMore.addEventListener("click", function () {
+    mainFind.style.display = "grid";
+    footer.style.display = "flex";
+    mainFind.scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+fSeeMore.addEventListener("click", function () {
+    mainShare.style.display = "grid";
+    footer.style.display = "flex";
+    mainShare.scrollIntoView({
+        behavior: "smooth"
+    });
+});
