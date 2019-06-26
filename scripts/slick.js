4b304a865a52d26de80546b4d104327279152f2a
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-26 03:39:30 -0400

    Customized Slick Features

--- scripts/slick.js
+++ scripts/slick.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     $('.slick-carousel').slick({
         dots: true,
-        infinite: false,
+        infinite: true,
         speed: 300,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false
     });
 });
