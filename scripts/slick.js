c097d54027ff57bc3a908ebaf005e054dc178b72
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-26 18:28:50 -0400

    Added faded carousel and small descriptions to about page

--- scripts/slick.js
+++ scripts/slick.js
@@ -7,11 +7,22 @@ $('.slick-carousel').slick({
     autoplaySpeed: 2500
 });
 
-$('.slick-carousel2').slick({
+//$('.slick-carousel2').slick({
 //    dots: true,
-    infinite: true,
-    slidesToShow: 1,
+//    infinite: true,
+//    slidesToShow: 1,
+//    arrows: false,
+//    autoplay: true,
+//    autoplaySpeed: 2500
+//});
+
+$('.slick-carousel2').slick({
+    dots: true,
     arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
     autoplay: true,
     autoplaySpeed: 2500
 });
