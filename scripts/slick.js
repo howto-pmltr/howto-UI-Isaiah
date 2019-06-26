0c8088008d3da65852ad083b81e33745d14bf0ed
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-26 19:28:21 -0400

    Added Arrows

--- scripts/slick.js
+++ scripts/slick.js
@@ -2,14 +2,13 @@ $('.slick-carousel').slick({
     dots: true,
     infinite: true,
     slidesToShow: 1,
-    arrows: false,
+    arrows: true,
     autoplay: true,
     autoplaySpeed: 1500
 });
 
 $('.slick-carousel2').slick({
-    dots: true,
-    arrows: false,
+    arrows: true,
     infinite: true,
     speed: 500,
     fade: true,
