d08dbe820ec59fe2d08a7e7adca978d6d3714b86
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-26 23:33:39 -0400

    Minified files to run site faster

--- about.js
+++ about.js
@@ -1,8 +1,1 @@
-// DOM Selectors
-const aboutMenu = document.querySelector(".aboutMenu");
-const menuItems = document.querySelector(".menuItems2");
-
-aboutMenu.addEventListener('click', function () {
-    menuItems.classList.toggle('toggleMenu2');
-    console.log("clicked")
-})
+const aboutMenu=document.querySelector(".aboutMenu");const menuItems=document.querySelector(".menuItems2");aboutMenu.addEventListener('click',function(){menuItems.classList.toggle('toggleMenu2')})
